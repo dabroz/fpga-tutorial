cdd4860d9104bf89dce32d07a15acf7a86e2c142
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-11-25 13:17:38 +0100

    ex 1

--- verilog/adder.v
+++ verilog/adder.v
@@ -5,3 +5,16 @@ module half_adder(input wire  x,
   xor(s, x, y);
   and(c_out, x, y);
 endmodule
+
+module adder(input wire x,
+             input wire y,
+             input wire c_in,
+             output wire s,
+             output wire c_out);
+  wire s_0;
+  wire c_0;
+  wire c_1;
+  half_adder add1(x, y, s_0, c_0);
+  half_adder add2(s_0, c_in, s, c_1);
+  or(c_out, c_0, c_1);
+endmodule
