f4b6267d91a09279b1a29efa928e3204aab74d47
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-11-25 13:37:03 +0100

    multibit

--- verilog/addermb.v
+++ verilog/addermb.v
@@ -20,10 +20,7 @@ module adder(input wire x,
 endmodule
 
 module adder4(input wire [3:0] x,
-              input wire y_0,
-              input wire y_1,
-              input wire y_2,
-              input wire y_3,
+              input wire [3:0] y,
               input wire c_in,
               output wire s_0,
               output wire s_1,
@@ -33,8 +30,8 @@ module adder4(input wire [3:0] x,
   wire c_0;
   wire c_1;
   wire c_2;
-  adder a_0(x[0:0], y_0, c_in, s_0, c_0);
-  adder a_1(x[1:1], y_1, c_0,  s_1, c_1);
-  adder a_2(x[2:2], y_2, c_1,  s_2, c_2);
-  adder a_3(x[3:3], y_3, c_2,  s_3, c_out);
+  adder a_0(x[0:0], y[0:0], c_in, s_0, c_0);
+  adder a_1(x[1:1], y[1:1], c_0,  s_1, c_1);
+  adder a_2(x[2:2], y[2:2], c_1,  s_2, c_2);
+  adder a_3(x[3:3], y[3:3], c_2,  s_3, c_out);
 endmodule
--- verilog/addermb_tb.v
+++ verilog/addermb_tb.v
@@ -2,24 +2,24 @@
 
 module Top;
   reg [3:0] x;
-  reg y_0, y_1, y_2, y_3;
+  reg [3:0] y;
   reg c_in;
   wire s_0, s_1, s_2, s_3;
   wire c_out;
 
-  adder4 a(x, y_0, y_1, y_2, y_3, c_in, s_0, s_1, s_2, s_3, c_out);
+  adder4 a(x, y, c_in, s_0, s_1, s_2, s_3, c_out);
 
   initial
     begin
-      $monitor($time, " x = %b, y = %b%b%b%b, c_in = %b, s = %b%b%b%b, c_out = %b",
-               x, y_0, y_1, y_2, y_3, c_in, s_0, s_1, s_2, s_3, c_out);
+      $monitor($time, " x = %b, y = %b, c_in = %b, s = %b%b%b%b, c_out = %b",
+               x, y, c_in, s_0, s_1, s_2, s_3, c_out);
       $dumpfile("adder_tb3.vcd");
       $dumpvars;
 
-      x = 0; y_0 = 0; y_1 = 0; y_2 = 0; y_3 = 0; c_in = 0;
-      #5 x = 1; y_0 = 0; y_1 = 0; y_2 = 1; y_3 = 0; c_in = 0;
-      #5 x = 5; y_0 = 0; y_1 = 1; y_2 = 0; y_3 = 1; c_in = 1;
-      #5 x = 15; y_0 = 0; y_1 = 0; y_2 = 0; y_3 = 0; c_in = 1;
+      x = 0; y = 0; c_in = 0;
+      #5 x = 1; y = 2; c_in = 0;
+      #5 x = 5; y = 5; c_in = 1;
+      #5 x = 15; y = 0; c_in = 1;
       #5 $finish;
     end
 endmodule // Top
