9c90493dff8c35d2adba34f5f6744341f0870c7a
Tomasz Dabrowski <t.dabrowski@rock-hard.eu>
2018-11-25 13:37:49 +0100

    multibit

--- verilog/addermb.v
+++ verilog/addermb.v
@@ -22,16 +22,13 @@ endmodule
 module adder4(input wire [3:0] x,
               input wire [3:0] y,
               input wire c_in,
-              output wire s_0,
-              output wire s_1,
-              output wire s_2,
-              output wire s_3,
+              output wire [3:0] s,
               output wire c_out);
   wire c_0;
   wire c_1;
   wire c_2;
-  adder a_0(x[0:0], y[0:0], c_in, s_0, c_0);
-  adder a_1(x[1:1], y[1:1], c_0,  s_1, c_1);
-  adder a_2(x[2:2], y[2:2], c_1,  s_2, c_2);
-  adder a_3(x[3:3], y[3:3], c_2,  s_3, c_out);
+  adder a_0(x[0:0], y[0:0], c_in, s[0:0], c_0);
+  adder a_1(x[1:1], y[1:1], c_0,  s[1:1], c_1);
+  adder a_2(x[2:2], y[2:2], c_1,  s[2:2], c_2);
+  adder a_3(x[3:3], y[3:3], c_2,  s[3:3], c_out);
 endmodule
--- verilog/addermb_tb.v
+++ verilog/addermb_tb.v
@@ -4,15 +4,15 @@ module Top;
   reg [3:0] x;
   reg [3:0] y;
   reg c_in;
-  wire s_0, s_1, s_2, s_3;
+  wire [3:0] s;
   wire c_out;
 
-  adder4 a(x, y, c_in, s_0, s_1, s_2, s_3, c_out);
+  adder4 a(x, y, c_in, s, c_out);
 
   initial
     begin
-      $monitor($time, " x = %b, y = %b, c_in = %b, s = %b%b%b%b, c_out = %b",
-               x, y, c_in, s_0, s_1, s_2, s_3, c_out);
+      $monitor($time, " x = %b, y = %b, c_in = %b, s = %b, c_out = %b",
+               x, y, c_in, s, c_out);
       $dumpfile("adder_tb3.vcd");
       $dumpvars;
 
